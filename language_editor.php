1400a540b3c8a1deabd862ec4593d5bfbcd7baef
Ryan Valentine <rval@stanford.edu>
2019-04-22 16:35:49 -0700

    more

---
+++ language_editor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Stanford\LanguageEditor;
+use \REDCap as REDCap;
+
+class language_editor extends \ExternalModules\AbstractExternalModule {
+
+    function hook_every_page_top($project_id) {
+
+    }
+
+
+
+}
