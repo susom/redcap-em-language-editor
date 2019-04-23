a8c5b1ad060e5e50fa7c8b7524337a930ac46fae
Ryan Valentine <rval@stanford.edu>
2019-04-22 22:01:08 -0700

    using , still bugged

--- LanguageEditor.php
+++ LanguageEditor.php
@@ -5,14 +5,6 @@ include_once ("emLoggerTrait.php");
 
 use \REDCap;
 
-/**
- *
- * A bunch of little CSS and JS tweaks that help enhance survey functionality
- * TODO: Make configuration available from the actual Survey Settings page
- *
- * Class SurveyUITweaks
- * @package Stanford\SurveyUITweaks
- */
 class LanguageEditor extends \ExternalModules\AbstractExternalModule
 {
     use emLoggerTrait;
@@ -22,8 +14,44 @@ class LanguageEditor extends \ExternalModules\AbstractExternalModule
         parent::__construct();
     }
 
-    public function redcap_every_page_before_render(){
-        $this -> emDebug(PAGE);
+    public function redcap_every_page_top(){
+        $this->emDebug(PAGE);
+
+        $updates = $this->getSubSettings('language_updates');
+        $this->emDebug($updates);
+
+        global $lang;
+
+        foreach ($updates as $update) {
+            $k = $update['language_key'];
+            $v = $update['language_value'];
+            $this->emDebug($k, $v);
+
+                if ($lang['key'] = $k)
+                {
+                    $lang['value'] = $v;
+                }
+
+           $this->emDebug($lang['key']);
+           $this->emDebug($lang['value']);
+
+            //this is just changing the key not the value
+            $lang[$update['language_key']] = $update['language_value'];
+            //$this->emDebug($lang[$update['language_key']]);
+
+
+            //$this->emDebug($GLOBALS['lang'][$k]);
+
+
+            //$GLOBALS['lang'][$update['language_key']] = $update['language_value'];
+
+
+            //$this->emDebug($GLOBALS['lang'][$k]);
+        }
+
+        //$this->emDebug(array_slice($GLOBALS['lang'], 0, 10));
+
+
     }
 
 
